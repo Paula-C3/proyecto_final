1f6826585617d7c0ae7fea2a30f4de6fc79230a7
Sorayaxoxo <153528733+Sorayaxoxo@users.noreply.github.com>
2023-12-11 10:47:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ df_streaming.head()
 
 # null values
 pd. isnull (df_streams).sum()
+
+df_streaming.info()
+
+sorted_df=df_streaming.sort_values('popularity', ascending=true).head(10)sorted_df
+
+df_streaming.describe().transpose 
