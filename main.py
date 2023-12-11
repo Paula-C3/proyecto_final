46b77b3ba5426e72c7db8a1a6c2c160830de05c2
Sorayaxoxo <153528733+Sorayaxoxo@users.noreply.github.com>
2023-12-11 13:19:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,45 @@ pd. isnull (df_streams).sum()
 
 df_streaming.info()
 
-sorted_df=df_streaming.sort_values('popularity', ascending=True).head(10)sorted_df
+sorted_df=df_streaming.sort_values('msPlayed', ascending=True).head(10)sorted_df
 
-df_streaming.describe().transpose 
+df_streaming.describe().transpose
 
-most_popular=df_streaming.query('popularity>90', inplace=False).sort_values('popularity', ascending=False)
+most_popular=df_streaming.query('popularity>10000', inplace=False).sort_values('msPlayer', ascending=False)
 most_popular[:10]
 
 df_streaming.set_index("release_date", inplace=True)
+
+
+#In[13]
+sample_df=df_streaming.sample(int(0.004*len(df_streaming)))
+#In[14]
+print(len(sample_df))
+
+#In[15]
+plt.figure(figsize=(10,6))
+sns.regplot(data=sample_df,y="msPlayer",x="artistName",color ="c").set(title="Loudness vs Energy Correlation")
+
+#In[16]
+#plt.figure(figuresize(10,6)
+#sns.regplot(data=sample_df,y="msPlayer",x="acousticness", color="b").set(title="msPlayer vs Acousticness Correlation")
+
+#In[21]
+#df_gender=pd.read_csv("/Users/paulacaicedo/Desktop/proyecto final/MyData copy/YourLibrary.json")
+#In[22]
+#df_gender.head()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
