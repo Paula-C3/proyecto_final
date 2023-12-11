27cd72ee6bfe89ae5a10f28c9ebda7cc4c7b24b7
Paula <139386553+Paula-C3@users.noreply.github.com>
2023-12-11 10:33:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns 
 
-df_streaming = pd.read_json('StreamingHistory0.json')
+df_streaming = pd.read_json(F:/Users/paulacaicedo/Desktop/proyecto final/StreamingHistory0.json')
 df_streaming.head()
