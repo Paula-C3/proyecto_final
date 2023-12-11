5399e92889a53d880dcfb15bacb9e7549e3b1960
vaishalidesai1 <153528879+vaishalidesai1@users.noreply.github.com>
2023-12-11 10:43:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ import seaborn as sns
 
 df_streaming = pd.read_json(/Users/paulacaicedo/Desktop/proyecto final/StreamingHistory0.json')
 df_streaming.head()
+
+# null values
+pd. isnull (df_streams).sum()
