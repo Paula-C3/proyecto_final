53a883f218e60e8ff5f610d4acaa0919e0c13103
Paula <139386553+Paula-C3@users.noreply.github.com>
2023-12-12 19:14:39 -0500

    Update comparar_canciones.py

--- comparar_canciones.py
+++ comparar_canciones.py
@@ -1,21 +1,24 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import base64
-from requests import post, get
-import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
-SPOTIPY_CLIENT_ID='e1379e41dc9545dfa6d3d005676da056'
-SPOTIPY_CLIENT_SECRET='6bad838d15514a2c903037b6ee6f5bbc'
+
+SPOTIPY_CLIENT_ID = 'e1379e41dc9545dfa6d3d005676da056'
+SPOTIPY_CLIENT_SECRET = '6bad838d15514a2c903037b6ee6f5bbc'
 
 auth_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 sp.me
- #------------------------------------------------------------------------------------------
-#-------------------------------comparación entre canciones------------------------------
-def palabra
+
+# ------------------------------------------------------------------------------------------
+# -------------------------------comparación entre canciones------------------------------
+
 palabra = input(print("Ingresa la palabra que crees que más se repite en los títulos de tus canciones: "))
 track_data = sp.search(q='track:'+'palabra',type='track', limit=20)
-return track_data
+track_result = []
 
 for i, item in enumerate(track_data['tracks']['items']):
     track = item['album']
@@ -23,37 +26,44 @@ for i, item in enumerate(track_data['tracks']['items']):
     track_name = item['name']
     popularity = item['popularity']
     track_result.append((i, track['artists'][0]['name'], track['name'], track_id, track_name, track['release_date'], popularity))
-return track_result
+track_data
+track_result = []
 
-track def = pd.DataFrame(track_result, index=None, columns=('Item', 'Artist', 'Album Name', 'Id', 'Song Name', 'Release Date', 'Popularity'))
-return track
+for i, item in enumerate(track_data['tracks']['items']):
+    track = item['album']
+    track_id = item['id']
+    track_name = item['name']
+    popularity = item['popularity']
+    track_result.append((i, track['artists'][0]['name'], track['name'], track_id, track_name, track['release_date'], popularity))
+track_result
+track_df = pd.DataFrame(track_result, index=None, columns=('Item', 'Artist', 'Album Name', 'Id', 'Song Name', 'Release Date', 'Popularity'))
+track_df
 
-features def = pd.DataFrame()
-for id in track_df['Id'].iteritems():
+features_df = pd.DataFrame()
+for id in track_df["Id"].items():
     track_id = id[1]
     audio_features = sp.audio_features(track_id)
     local_features = pd.DataFrame(audio_features, index=[0])
-    features def = features_df.append(local_features)
-return features
+    features_df = features_df._append(local_features)
 
-final def = track_df.merge(features_df, left_on="Id", right_on="id")
-return final
+features_df
 
-final def sorted = final_df.sort_values(by=['Popularity'], ascending=False)
-return final sorted
+final_df = track_df.merge(features_df, left_on="Id", right_on="id")
+final_df
 
-def plot = final_df_sorted[['Artist', 'Album Name', 'Song Name', 'Release Date', 'Popularity', f'{feature_name}']]
-return def
+final_df_sorted = final_df.sort_values(by=['Popularity'], ascending=False)
+final_df_sorted
 
-#gráfica
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
+feature_name = "liveness"
+
+df_plot = final_df_sorted[['Artist', 'Album Name', 'Song Name', 'Release Date', 'Popularity', f'{feature_name}']]
+df_plot
+
 plt.figure(figsize=(15, 6), facecolor=(.9, .9, .9))
     
-x = def plot['Song Name']
-y = def plot[feature_name]
-s = def plot['Popularity']*30
+x = df_plot['Song Name']
+y = df_plot[feature_name]
+s = df_plot['Popularity']*30
     
 # color_labels = reco_df['explicit'].unique()
 # rgb_values = sns.color_palette("Set1", 8)
